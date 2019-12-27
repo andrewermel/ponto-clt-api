494f8ddbb35f84a0965e5cc9a1563130c52b01ed
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-12-27 03:00:38 -0300

    Add open cors

--- web/utilidades.php
+++ web/utilidades.php
@@ -8,3 +8,10 @@ function conecta(){
 	$conexao= mysqli_connect($maquina,$usuario,$senha,$database);
 	return $conexao;
 }
+
+function openCors(){
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: *');
+    header('Access-Control-Allow-Methods: *');
+}
+openCors();
